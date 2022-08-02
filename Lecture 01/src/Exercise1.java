353914837d9242e1d7c819a3aaa91aa942ef28a6
Ishara Madusanka <isharamadusanka410@gmail.com>
2022-08-02 21:20:42 +0530

    created  lecture 1 folder

--- Lecture 01/src/Exercise1.java
+++
@@ -1,9 +0,0 @@
-
-public class Exercise1 {
-	public static void main(String []args){
-		System.out.println("Ishara Madusanka");
-		System.out.println("Lake view, Sri Soratha Mawatha, Nakanda");
-		System.out.print("Hikkaduwa");
-
-	}
-}
--- src/module-info.java
+++
@@ -1,2 +0,0 @@
-module Lecture_01 {
-}
