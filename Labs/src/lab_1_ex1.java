93aceba368be8acf888a8f6787ca418a3283914b
Ishara Madusanka <isharamadusanka410@gmail.com>
2022-08-03 09:56:23 +0530

    Exercise 1 for lab 1 done

---
+++ Labs/src/lab_1_ex1.java
@@ -0,0 +1,7 @@
+
+public class lab_1_ex1 {
+		public static void main (String []args) {
+			System.out.println("Hello world!");
+			System.out.print("It's been nice knowing you. \nGoodbye world!");
+		}
+}
