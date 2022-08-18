aa7f1c32bb273f12ea3931af2cd5a50c64b648ef
Ishara Madusanka <isharamadusanka410@gmail.com>
2022-08-18 12:22:03 +0530

    implemented question 2 to generate random number array

---
+++ Assignment/src/MyMain.java
@@ -0,0 +1,83 @@
+
+import java.util.Arrays;
+import java.util.Random;
+
+
+class RandNum{
+	int[][] array = new int[5][5];
+	
+	public RandNum() {
+		int i,k , rand;
+		double random;
+		
+		
+		for(i = 0; i <5; i++) {
+			for(k = 0; k < 5;k++) {
+				random = Math.random() * 100;
+				rand = (int)random + 1 ;
+				array[i][k] = rand;
+			}
+		}
+		
+	}
+	public void showArr() {
+
+		int i,k , rand;
+		double random;
+		
+		
+		for(i = 0; i <5; i++) {
+			for(k = 0; k < 5;k++) {
+				System.out.print(" " + array[i][k] + " ");
+			}
+			System.out.println();
+		}
+		
+	}
+	
+	public void calculate() {
+		int i, k, x;
+		int maximum = 0, minimum = 0;
+		double avg;
+		int total = 0;
+		int[] store = {array[0][0],array[0][0],array[0][0]};
+		
+		for(i = 0; i < 5; i++ ) {
+			for(k = 0;k < 5; k++) {
+				
+				x = array[i][k];
+				
+				//check the maximum value
+				if(store[0] <= x) {
+					maximum = x;
+					store[0] = x;
+				}
+				
+				//check the minimum value
+				if(store[1] >= x) {
+					minimum = x;
+					store[1] = x;
+				}
+				
+				//store the sum of all values in the array
+				total += x;
+			}
+		}
+		
+		avg = total/25.0;
+		
+		System.out.println("Maximum is " + maximum);
+		System.out.println("Minimum is " + minimum);
+		System.out.println("Average is " + avg);
+		
+		
+	}
+}
+
+public class MyMain {
+	public static void main(String[] args) {
+		RandNum num = new RandNum();
+		num.showArr();
+		num.calculate();
+	}
+}
