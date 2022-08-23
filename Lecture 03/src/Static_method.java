ba169e17d1f552878937efed973dc96f96178a0e
Ishara Madusanka <isharamadusanka410@gmail.com>
2022-08-23 22:31:50 +0530

    implemented main for the static method.

---
+++ Lecture 03/src/Static_method.java
@@ -0,0 +1,13 @@
+
+public class Static_method {
+	private static int age;
+	private static String name;
+	
+	public static void details(int a, String n) {
+		age = a;
+		name = n;
+		
+		System.out.println(age);
+		System.out.println(name);
+	}
+}
---
+++ Lecture 03/src/Static_method_main.java
@@ -0,0 +1,6 @@
+
+public class Static_method_main {
+	public static void main(String[] args) {
+		Static_method.details(24, "Ishara");
+	}
+}
