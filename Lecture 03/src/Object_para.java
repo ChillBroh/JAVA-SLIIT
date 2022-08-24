f76d8d7f87c5b657b1e1749783d10f45b66ad6cb
Ishara Madusanka <isharamadusanka410@gmail.com>
2022-08-24 19:21:33 +0530

    implemented class and added method and object parameter

---
+++ Lecture 03/src/Object_para.java
@@ -0,0 +1,33 @@
+
+public class Object_para {
+	String name;
+	int age;
+	double sal;
+	
+	
+	
+	public Object_para() {
+		super();
+	}
+	public Object_para(String name, int age, double sal) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.sal = sal;
+	}
+	void show() {
+
+		System.out.println("name is " + name);
+		System.out.println("age is " + age);
+		System.out.println("Salary is " + sal);
+	}
+	void show(Object_para x) {
+		System.out.println("name is " + name);
+		System.out.println("age is " + age);
+		System.out.println("Salary is " + sal);
+	}
+	
+
+
+
+}
