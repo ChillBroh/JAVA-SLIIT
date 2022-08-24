7c7e6d46ba7a8529ff83b7ce0211b63342f0699d
Ishara Madusanka <isharamadusanka410@gmail.com>
2022-08-24 19:47:01 +0530

    added the values for p2 object and passed it using show method

--- Lecture 03/src/object_paraMain.java
+++ Lecture 03/src/object_paraMain.java
@@ -2,13 +2,13 @@
 public class object_paraMain {
 	public static void main(String[] args) {
 		Object_para p1 = new Object_para("Kamal", 25, 8000.0);
-		Object_para p2 = new Object_para();
+		Object_para p2 = new Object_para("Raja",26,85.2);
 		
 		p1.show();
 		System.out.println("********************");
-		p1.show(p2);
-		System.out.println("********************");
 		p2.show(p1);
+		System.out.println("********************");
+		p1.show(p2); 
 		
 	}
 }
