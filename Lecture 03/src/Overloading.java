20d43c1533d0999267a45fc6cd91f567eff85c13
Ishara Madusanka <isharamadusanka410@gmail.com>
2022-08-28 00:10:57 +0530

    created same name Methods with different parameters

---
+++ Lecture 03/src/Overloading.java
@@ -0,0 +1,16 @@
+
+public class Overloading {
+	public void display(int a, int b) {
+		System.out.println("Method A");
+	}
+	public void display(int a, double b, double c) {
+		System.out.println("Method B");
+	}
+	public void display(int a, float b) {
+		System.out.println("Method C");
+	}
+	public void display(int a, char b) {
+		System.out.println("Method D");
+	}
+	
+}
