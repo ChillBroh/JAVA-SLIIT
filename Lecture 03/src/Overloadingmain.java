cfc4d2576c4722675c3570e5f5afbfe369ef6918
Ishara Madusanka <isharamadusanka410@gmail.com>
2022-08-28 00:11:30 +0530

    called the methods in overloading file

---
+++ Lecture 03/src/Overloadingmain.java
@@ -0,0 +1,10 @@
+
+public class Overloadingmain {
+	public static void main(String[] args) {
+		Overloading ob = new Overloading();
+		ob.display(100,20.5f);
+		ob.display(52,'y');
+		ob.display(5,6);
+		
+	}
+}
