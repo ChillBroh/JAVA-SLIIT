4c5162ffc7e61b431222958d2282783114bdecbb
Ishara Madusanka <isharamadusanka410@gmail.com>
2022-08-28 00:37:35 +0530

    added constructor

--- Lecture 03/src/Circle.java
+++ Lecture 03/src/Circle.java
@@ -2,7 +2,9 @@
 
 public class Circle extends Shape{
 	private int r;
-	
+	Circle(int radius){
+		this.r = radius;
+	}
 	public int calcArea() {
 		return (int)(2 * 3.14 * r *r) ; 
 	}
--- Lecture 03/src/Rectangle.java
+++ Lecture 03/src/Rectangle.java
@@ -1,7 +1,10 @@
 
 public class Rectangle extends Shape{
 private int len, wid;
-	
+	Rectangle(int l, int w){
+		this.len = l;
+		this.wid = w;
+	}
 	public int calcArea() {
 		return len * wid;
 	}
