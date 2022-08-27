416a91175db910467c42a7615dcd430098c8fa0a
Ishara Madusanka <isharamadusanka410@gmail.com>
2022-08-28 00:40:49 +0530

    creted objects from all the classes and called the calc method

--- Lecture 03/src/ShapeMain.java
+++ Lecture 03/src/ShapeMain.java
@@ -2,6 +2,14 @@
 public class ShapeMain {
 	public static void main(String[] args) {
 		Shape ob = new Shape();
-		ob.calcArea();
+		System.out.println(ob.calcArea());
+	
+		Circle ob1 = new Circle(7);
+		System.out.println(ob1.calcArea());
+		
+		Rectangle ob2 = new Rectangle(5, 6);
+		System.out.println(ob2.calcArea());
+		
 	}
 }
+
