9a90d43e06633eb258ba0e13d3bdcab7ca1595a6
Ishara Madusanka <isharamadusanka410@gmail.com>
2022-08-28 00:35:13 +0530

    removed constructor

--- Lecture 03/src/Rectangle.java
+++ Lecture 03/src/Rectangle.java
@@ -1,10 +1,7 @@
 
 public class Rectangle extends Shape{
 private int len, wid;
-	Rectangle(int l, int w){
-		this.len = l;
-		this.wid = w;
-	}
+	
 	public int calcArea() {
 		return len * wid;
 	}
