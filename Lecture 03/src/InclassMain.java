32961c4d7581fafbb6b165c557d9bafa5bab69c7
Ishara Madusanka <isharamadusanka410@gmail.com>
2022-08-27 23:32:06 +0530

    object paramterized constructer called using my box 2

--- Lecture 03/src/InclassMain.java
+++ Lecture 03/src/InclassMain.java
@@ -4,7 +4,15 @@ public class InclassMain {
 	public static void main(String[] args) {
 		Inclass mybox = new Inclass(10,20,30);
 		
+		Inclass mybox2 = new Inclass(mybox);
 		
+		double vol;
+		
+		vol = mybox.volume();
+		System.out.println("my box volume is " + vol);
+		
+		vol = mybox2.volume();
+		System.out.println("My box 2 volume is " + vol);
 		
 	}
 }
