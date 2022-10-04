88f1626ca8ffb8dc0db368bdbc3ddbeaeb5d33e7
Ishara Madusanka <isharamadusanka410@gmail.com>
2022-10-04 22:53:12 +0530

    try catch first time

---
+++ Lecture 03/src/Lec_5/coding_ex1.java
@@ -0,0 +1,9 @@
+package Lec_5;
+
+public class coding_ex1 {
+	public static void main(String[] args) {
+	
+		System.out.println("hello");
+		int x = Integer.parseInt("hello");
+	}
+}
--- Lecture 03/src/interface_lec/Interface01.java
+++ Lecture 03/src/interface_lec/Interface01.java
@@ -4,10 +4,10 @@ public interface Interface01 {
 	// all methods are abstract. no concrete
 	// no need to right abstract
 	
-	final int age = 45;
+	static int age = 45;
 	public int getArea();
 	//no want to write as abstract public int getArea() cause interface methods are always abstract
 	
-	public int getPerimeter(int x);
+	public int  getPerimeter(int x);
 	void display();
 }
