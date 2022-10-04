382385b605ace0160a60c3dd3a834b93e37fb1cd
Ishara Madusanka <isharamadusanka410@gmail.com>
2022-10-04 16:38:55 +0530

    implemented shape class in separate file

---
+++ Lecture 03/src/lec_4/Shape.java
@@ -0,0 +1,16 @@
+package lec_4;
+
+public class Shape {
+	
+		public void show() {
+			//hlf
+		}
+		public int getArea() {
+			return 0;
+		}
+	
+
+	public class Rectangle extends Shape{
+		
+	}
+}
