b2e3a2a11d01361ff1fcdf082de72d2a9f6ddac2
Ishara Madusanka <isharamadusanka410@gmail.com>
2022-10-04 18:54:30 +0530

    created interface and aded methods and attributes

---
+++ Lecture 03/src/interface_lec/Interface01.java
@@ -0,0 +1,11 @@
+package interface_lec;
+
+public interface Interface01 {
+	// all methods are abstract. no concrete
+	// no need to right abstract
+	
+	final int age = 45;
+	public int getArea();
+	//no want to write as abstract public int getArea() cause interface methods are always abstract
+	
+}
