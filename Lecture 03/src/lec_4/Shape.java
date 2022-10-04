bebdd3dcac6b9832fc79d237ea39beadeae7cce7
Ishara Madusanka <isharamadusanka410@gmail.com>
2022-10-04 16:41:47 +0530

    added abstract keyword

--- Lecture 03/src/lec_4/Shape.java
+++ Lecture 03/src/lec_4/Shape.java
@@ -1,13 +1,15 @@
 package lec_4;
 
-public class Shape {
+abstract public class Shape { //one method in abstract class is abstract so we must use abastract keyword to implement class
 	
 		public void show() {
 			//hlf
 		}
-		public int getArea() {
-			return 0;
-		}
+		
+		abstract public int getArea();
+//		public int getArea() {
+//			return 0;
+//		}
 	
 
 	public class Rectangle extends Shape{
