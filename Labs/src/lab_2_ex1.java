8dc849c9552ab3e5fb9254d668482872a49b3bd1
Ishara Madusanka <isharamadusanka410@gmail.com>
2022-11-27 01:38:21 +0530

    lab2 ex1 done

---
+++ Labs/src/lab_2_ex1.java
@@ -0,0 +1,15 @@
+
+public class lab_2_ex1 {
+	public static void main(String[] args) {
+		int miles = 26;
+		int yards = 385;
+		double kilometers;
+		
+		 
+		
+		kilometers = (yards*1.609/1760.0) + (miles * 1.609);
+		System.out.println(kilometers);
+		
+		
+	}
+}
