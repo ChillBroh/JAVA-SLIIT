76b67105d0c8226af1945de18bf17b915f381a57
Ishara Madusanka <isharamadusanka410@gmail.com>
2022-11-27 23:53:36 +0530

    initial commit

--- Lecture 09/src/synchro_block/Document.java
+++ Lecture 09/src/synchro_block/Document.java
@@ -5,14 +5,14 @@ public class Document {
 		System.out.println("print");
 		System.out.println("print2");
 		
-		synchronized (ob) {
+		synchronized (ob) { //also can use (new Document()) but this will remove synchro. cause syncro is many threads are tryng to access one object. this will create another object
 			for(int i= 0; i < 10; i++) {
 				System.out.println("Updating doc " + i);
 				
 				try {
 					Thread.sleep(1000);
 				} catch (Exception e) {
-					e.getMessage();
+					e.getMessage(); 
 				}
 			}
 		}
